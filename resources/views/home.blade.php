1d6191819a0e9bf0e3fd6027250c8b6309279ce7
Falco13 <keanu0608@gmail.com>
2024-07-16 22:24:29 +0300

    fix home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,22 +3,22 @@
 @section('content')
 <br>
 <div class="container-fluid">
-<form method="GET" action="{{ route('search') }}" class="d-flex">
-    <input class="form-control me-2" id="s" name="s" type="search" placeholder="Search by name">
-    <button class="btn btn-warning" type="submit">Search</button>
-</form>
+    <form method="GET" action="{{ route('search') }}" class="d-flex">
+        <input class="form-control me-2" id="s" name="s" type="search" placeholder="Search by name">
+        <button class="btn btn-warning" type="submit">Search</button>
+    </form>
 </div>
 <br>
 
 @if(count($users))
 <table class="table table-striped">
     <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Name</th>
-        <th scope="col">Email</th>
-        <th scope="col">Phone</th>
-      </tr>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+        </tr>
     </thead>
     <tbody>
         @foreach ($users as $user)
@@ -28,13 +28,13 @@
             <td>{{ $user->email }}</td>
             <td>{{ $user->phone }}</td>
         </tr>
-      @endforeach
+        @endforeach
     </tbody>
-  </table>
+</table>
 {{-- {{ $users->links() }} --}}
 {{ $users->appends(['s'=>request()->s])->links() }}
 <br>
 @else
-  <h3>There are no entries...</h3>
+    <h3>There are no entries...</h3>
 @endif
 @endsection
